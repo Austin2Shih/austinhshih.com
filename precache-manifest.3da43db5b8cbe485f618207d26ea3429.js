44d330d6ce83dd712d8caa89e800be0a6a35e5c6
Austin Shih <austinhshih@gmail.com>
2022-09-01 22:43:12 -0700

    Updates

---
+++ precache-manifest.3da43db5b8cbe485f618207d26ea3429.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "e629651e37321d2399a8",
+    "url": "austinshih.com/static/css/main.9099da8f.chunk.css"
+  },
+  {
+    "revision": "e629651e37321d2399a8",
+    "url": "austinshih.com/static/js/main.e629651e.chunk.js"
+  },
+  {
+    "revision": "afa1c548182689d7118b",
+    "url": "austinshih.com/static/js/1.afa1c548.chunk.js"
+  },
+  {
+    "revision": "99463745a2dbc1fc851b",
+    "url": "austinshih.com/static/js/runtime~main.99463745.js"
+  },
+  {
+    "revision": "570dc8173316e2e36db56f5cd26fc54e",
+    "url": "austinshih.com/static/media/city-photo.570dc817.jpg"
+  },
+  {
+    "revision": "380917068109983bc5edaa7b1a69184e",
+    "url": "austinshih.com/static/media/hawaii-pic.38091706.jpg"
+  },
+  {
+    "revision": "8a96058a71d47293059467ce69662b28",
+    "url": "austinshih.com/static/media/TortlePoll.8a96058a.PNG"
+  },
+  {
+    "revision": "fb0380e504f09878fdc9ee2f4bd5b751",
+    "url": "austinshih.com/static/media/falling block game image.fb0380e5.jpg"
+  },
+  {
+    "revision": "70fb1db9218957a897ee442efc02365a",
+    "url": "austinshih.com/static/media/ring-detection.70fb1db9.png"
+  },
+  {
+    "revision": "f070e7c555f28aa727e2722c392265f4",
+    "url": "austinshih.com/index.html"
+  }
+];
